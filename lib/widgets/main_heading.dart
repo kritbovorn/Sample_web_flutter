2d81acef65ef6f3c04fe304eee43f5038f7bed12
Kritbovorn <ktaweeyossak@yahoo.com>
2022-02-28 19:55:43 +0700

    layout MainHeading Text()

--- lib/widgets/main_heading.dart
+++ lib/widgets/main_heading.dart
@@ -9,7 +9,27 @@ class MainHeading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return screenSize.width < 800 
+    ? Padding(
+      padding: const EdgeInsets.only(top: 50, bottom: 20),
+      child: Row(
+        children: [
+
+          const Expanded(child: SizedBox()),
+
+          Expanded(
+            flex: 15,
+            child: Text(
+              "Knowledge diversity",
+              style: GoogleFonts.lato(fontSize: 32, color: Colors.white, fontWeight: FontWeight.bold),),
+          ),
+
+          const Expanded(child: SizedBox()),
+
+        ],
+      ),
+    )
+    : Padding(
       padding: EdgeInsets.only(
         top: screenSize.height / 10,
         bottom: screenSize.height / 15,
